b676e7c2a6043e64e3d34dac18785d2b7b66c2fb
Sanyia Saidova <sanyia.saidova@gmail.com>
2022-09-11 23:02:23 +0000

    Fixing post-generate hook for web

--- web/hooks/post_gen_project.py
+++ web/hooks/post_gen_project.py
@@ -4,13 +4,11 @@ import subprocess
 CURR_DIR = os.getcwd()
 PROJECT_DIR = os.path.abspath(f"{CURR_DIR}/../../.projects/{{ cookiecutter.repo_name }}")
 
-sub_env = {
-    "GH_TOKEN": os.environ.get("GH_TOKEN", "doesnotexist")
-}
-
 os.chdir(PROJECT_DIR)
 
-subprocess.run(['git', '-C', '{{ cookiecutter.repo_name }}', 'init'])
+sub_env = os.environ.copy() 
+
+subprocess.run(['git', 'init'])
 
 subprocess.run([
     'gh', 'repo', 'create', 
