adcc675d8c6522a99c5cddd061b6c133e3e19e52
Sanyia Saidova <sanyia.saidova@gmail.com>
2023-09-10 16:56:33 +0000

    update: cookiecutter arguments and default answers

--- templates/workflow-builder/hooks/post_gen_project.py
+++ templates/workflow-builder/hooks/post_gen_project.py
@@ -2,24 +2,24 @@ import os
 import subprocess
 import sys
 
-{% if cookiecutter.init_commit not in ["yes", "no"] %}
-sys.exit("init_commit must be yes or no")
+{% if cookiecutter.add_initial_commit not in ["y", "n"] %}
+sys.exit("add_initial_commit must be y or n")
 {% endif %}
 
-{% if cookiecutter.create_gh_repo not in ["yes", "no"] %}
-sys.exit("create_gh_repo must be yes or no")
+{% if cookiecutter.create_gh_repo not in ["y", "n"] %}
+sys.exit("create_gh_repo must be y or n")
 {% endif %}
 
 sub_env = os.environ.copy() 
 
-{% if cookiecutter.init_commit == "yes" %}
+{% if cookiecutter.add_initial_commit == "y" %}
 subprocess.run(['git', 'init'])
 subprocess.run(['git', 'branch', '-m', 'main'])
 subprocess.run(['git', 'add', '.'])
 subprocess.run(['git', 'commit', '-m', 'Initial commit - auto generated from cookiecutter'])
 {% endif %}
 
-{% if cookiecutter.create_gh_repo == "yes" %}
+{% if cookiecutter.create_gh_repo == "y" %}
 subprocess.run([
     'gh', 'repo', 'create', 
     '--source', '.',
